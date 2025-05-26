f8b574ecf40c9fb7eb6d520e5950e48d4a1b540a
Mark Peters <mapeters101@earthlink.net>
2025-05-26 18:03:49 -0400

    Work on lesson021 to 4:00.

---
+++ lesson021.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def whatIEat(self):
+        pass
+
+class Lion(Animal):
+    def whatIEat(self):
+        print("Lion eats meat")
+
+class Cow(Animal):
+    def whatIEat(self):
+        print("Cow eats grass")
+
+class Dog(Animal):
+    def whatIEat(self):
+        print("Dog eats dog food and drinks milk.")
+
+
+
+
